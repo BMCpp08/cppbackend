7c5801042ab96fefaef044bbb6a11b3a1087f16c
Matvei <bykov@ITL-NPP.local>
2026-02-16 19:44:08 +0300

    Создан проект htmldecode

---
+++ sprint3/problems/htmldecode/solution/src/htmldecode.cpp
@@ -0,0 +1,45 @@
+﻿#include "htmldecode.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std::literals;
+
+const std::unordered_map<std::string, std::string> html_entities = { {"&lt"s,"<"},
+    {"&LT"s,"<"},
+    {"&gt"s,">"},
+    {"&GT"s,">"},
+    {"&amp"s,"&"},
+    {"&AMP"s,"&"},
+    {"&apos"s,"\'"},
+    {"&APOS"s,"\'"},
+    {"&quot"s,"\""},
+    {"&QUOT"s,"\""} };
+
+std::string HtmlDecode(std::string_view str) {
+    std::string res;
+
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '&') {
+            bool replaced = false;
+            for (auto& entities : html_entities) {
+
+                const std::string& key = entities.first;
+                if (str.compare(i, key.size(), key) == 0) {
+                    res += entities.second;
+                    i += ((i + key.size()) < str.size() && str[i + key.size()] == ';') ? key.size() : key.size() - 1;
+                    replaced = true;
+                    break;
+                }
+            }
+            if (!replaced) {
+                res += str[i];
+            }
+        }
+        else {
+            res += str[i];
+        }
+    }
+
+    return res;
+}
---
+++ sprint3/problems/htmldecode/solution/tests/tests.cpp
@@ -0,0 +1,27 @@
+﻿#include <catch2/catch_test_macros.hpp>
+
+#include "../src/htmldecode.h"
+
+using namespace std::literals;
+//Строка без HTML-мнемоник.
+TEST_CASE("Text without mnemonics", "[HtmlDecode]") {
+    CHECK(HtmlDecode("hello"sv) == "hello"s);
+}
+
+//Строка с HTML - мнемониками.
+TEST_CASE("Text with mnemonics", "[HtmlDecode]") {
+    CHECK(HtmlDecode("I&ampDog, I&aposm Dog, I go &gt, I go &lt, &quotDog&quot"sv) == "I&Dog, I'm Dog, I go >, I go <, \"Dog\""s);
+    CHECK(HtmlDecode("I&amp;Dog, I&apos;m Dog, I go &gt;, I go &lt;, &quot;Dog&quot;"sv) == "I&Dog, I'm Dog, I go >, I go <, \"Dog\""s);
+    CHECK(HtmlDecode("I&AMPDog, I&APOSm Dog, I go &GT, I go &LT, &QUOTDog&QUOT"sv) == "I&Dog, I'm Dog, I go >, I go <, \"Dog\""s);
+    CHECK(HtmlDecode("I&AMP;Dog, I&APOS;m Dog, I go &GT;, I go &LT;, &QUOT;Dog&QUOT;"sv) == "I&Dog, I'm Dog, I go >, I go <, \"Dog\""s);
+}
+
+//Пустая строка.
+TEST_CASE("Text empty", "[HtmlDecode]") {
+    CHECK(HtmlDecode(""sv) == ""s);
+}
+
+//Строка с недописанными HTML-мнемониками
+TEST_CASE("A line with unwritten HTML mnemonics", "[HtmlDecode]") {
+    CHECK(HtmlDecode("I&amDog, I&apo Dog, I go &g, I go &t, &quoDog&quo"sv) == "I&amDog, I&apo Dog, I go &g, I go &t, &quoDog&quo"s);
+}
