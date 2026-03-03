e592f710a323456951ff7b83e7b75f740993566d
Matvei <bykov@ITL-NPP.local>
2026-03-03 21:23:53 +0300

    Signed-off-by: Matvei <bykov@ITL-NPP.local>

--- sprint4/problems/db_of_books/solution/main.cpp
+++ sprint4/problems/db_of_books/solution/main.cpp
@@ -150,7 +150,7 @@ int main(int argc, const char* argv[]) {
 								json::array arr;
 								pqxx::read_transaction r(conn);
 								auto rows = r.query<std::optional<int>, std::optional<std::string>, std::optional<std::string>, std::optional<int>, std::optional<std::string>>("SELECT id, title, author, year, isbn FROM books ORDER BY ORDER BY year DESC, title ASC, author ASC, isbn ASC;"_zv);
-								for (auto [id, title, author, year, ISBN] : rows) {
+								for (auto [id, title, author, year, ISBN] : r.query<std::optional<int>, std::optional<std::string>, std::optional<std::string>, std::optional<int>, std::optional<std::string>>("SELECT id, title, author, year, isbn FROM books ORDER BY ORDER BY year DESC, title ASC, author ASC, isbn ASC;"_zv)) {
 									arr.emplace_back(json::object{ {"id", id.value_or(-9999)},
 																				{"title", title.value_or("")},
 																				{"author", author.value_or("")},
