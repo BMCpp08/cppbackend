102cf452a0c7835745102ebc10165e7976fb269e
Matvei <bykov@ITL-NPP.local>
2026-03-04 20:04:07 +0300

    Signed-off-by: Matvei <bykov@ITL-NPP.local>

--- sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -61,13 +61,15 @@ CREATE TABLE IF NOT EXISTS authors (
     name varchar(100) UNIQUE NOT NULL
 );
 )"_zv);
-     /*work.commit();*/
+     work.commit();
     // ... создать другие таблицы
-    work.exec(R"(
-CREATE TABLE IF NOT EXISTS books (
+    work.exec(
+        R"(CREATE TABLE IF NOT EXISTS books (
         id UUID CONSTRAINT book_id_constraint PRIMARY KEY, 
+        author_id UUID NOT NULL, 
         title varchar(100) NOT NULL, 
-        publication_year integer NOT NULL
+        publication_year integer NOT NULL,
+        CONSTRAINT f_books FOREIGN KEY(author_id) REFERENCES authors(id)
 );
 )"_zv);
 
