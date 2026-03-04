567e60f109b7b20c57ddc75fa78cc2428ca50632
Matvei <bykov@ITL-NPP.local>
2026-03-04 19:50:21 +0300

    Signed-off-by: Matvei <bykov@ITL-NPP.local>

--- sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
+++ sprint4/problems/bookypedia-1/solution/src/postgres/postgres.cpp
@@ -68,8 +68,7 @@ CREATE TABLE IF NOT EXISTS authors (
         id UUID CONSTRAINT book_id_constraint PRIMARY KEY, 
         author_id UUID NOT NULL, 
         title varchar(100) NOT NULL, 
-        publication_year integer NOT NULL,
-        CONSTRAINT f_books FOREIGN KEY(author_id) REFERENCES authors(id);)"_zv);
+        publication_year integer NOT NULL;)"_zv);
 
     //// коммитим изменения
     work.commit();
