f8eb1f33f898f7b98a7144e987c403c96fa51276
Matvei <bykov@ITL-NPP.local>
2026-03-15 02:50:22 +0300

    Создание проекта leave_game

---
+++ sprint4/problems/leave_game/solution/src/postgres/postgres.cpp
@@ -0,0 +1,29 @@
+﻿#include "postgres.h"
+#include <iostream>
+#include <pqxx/pqxx>
+namespace postgres {
+	using namespace std::literals;
+	using pqxx::operator"" _zv;
+
+	Database::Database(const std::string& db_url) {
+		pqxx::connection conn(db_url);
+		pqxx::work work{ conn };
+		try {
+			work.exec(R"(
+			CREATE TABLE IF NOT EXISTS retired_players (
+					id UUID CONSTRAINT id_constraint PRIMARY KEY,
+					player_name TEXT NOT NULL,
+					score DOUBLE PRECISION NOT NULL,
+					play_time DOUBLE PRECISION NOT NULL
+			);
+			)"_zv);
+			work.exec(R"(CREATE INDEX IF NOT EXISTS idx_retired_players
+				ON retired_players(score DESC, play_time ASC, player_name ASC);)"_zv);
+			
+			work.commit();
+		}
+		catch (...) {
+			std::cerr << "Database constructor: error" << std::endl;
+		}
+	}
+}  // namespace postgres
