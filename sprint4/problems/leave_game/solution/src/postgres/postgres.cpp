fa027e171df5717ac2a3c6b598000a7041076886
Matvei <bykov@ITL-NPP.local>
2026-03-14 13:20:37 +0300

    Signed-off-by: Matvei <bykov@ITL-NPP.local>

--- sprint4/problems/leave_game/solution/src/postgres/postgres.cpp
+++ sprint4/problems/leave_game/solution/src/postgres/postgres.cpp
@@ -8,21 +8,28 @@ namespace postgres {
 	Database::Database(const std::string& db_url) {
 		pqxx::connection conn(db_url);
 		pqxx::work work{ conn };
-
-		//Таблица игры
-		work.exec(R"(
-		CREATE TABLE IF NOT EXISTS retired_players (
-			    id UUID CONSTRAINT id_constraint PRIMARY KEY,
-				player_name TEXT NOT NULL,
-				score DOUBLE PRECISION NOT NULL,
-				play_time_ms BIGINT NOT NULL
-		);
-		)"_zv);
-		work.exec(R"(CREATE INDEX IF NOT EXISTS idx_retired_players
+		try {
+			work.exec(R"(
+			CREATE TABLE IF NOT EXISTS retired_players (
+					id UUID CONSTRAINT id_constraint PRIMARY KEY,
+					player_name TEXT NOT NULL,
+					score DOUBLE PRECISION NOT NULL,
+					play_time_ms BIGINT NOT NULL
+			);
+			)"_zv);
+			work.exec(R"(CREATE INDEX IF NOT EXISTS idx_retired_players
 			ON retired_players(score DESC, play_time_ms, player_name);)"_zv);
 
-		work.commit();
+			work.commit();
+			std::cerr << "Tables created successfully" << std::endl;
 
+			std::cerr << "Database constructor: end" << std::endl;
+		}
+		catch (...) {
+			std::cerr << "Database constructor: error" << std::endl;
+		}
+		//Таблица игры
+		
 		pool_.PrepareAllConnections(db_url, 100);
 	}
 }  // namespace postgres
