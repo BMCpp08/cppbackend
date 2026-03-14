fffb4f417173eba721deef50a26bb18cf6628ca0
Matvei <bykov@ITL-NPP.local>
2026-03-14 21:41:50 +0300

    Signed-off-by: Matvei <bykov@ITL-NPP.local>

--- sprint4/problems/leave_game/solution/src/app/connection_pool_impl.cpp
+++ sprint4/problems/leave_game/solution/src/app/connection_pool_impl.cpp
@@ -24,12 +24,16 @@ void ConnectionPoolImpl::PrepareAllConnections(const std::string& db_url, const 
     
     pool_ = std::make_unique<ConnectionPool>(size, [db_url] {
         auto conn = std::make_shared<pqxx::connection>(db_url);
+        // Вставка записи о вышедшем на пенсию игроке
         conn->prepare(tag_ins_retired,
             "INSERT INTO retired_players (id, player_name, score, play_time) VALUES ($1, $2, $3, $4)"_zv);
 
-        conn->prepare("get_records",
-            "SELECT id, player_name AS name, score, play_time_ms"
-            "FROM retired_players ORDER BY score DESC, play_time_ms, player_name LIMIT $1 OFFSET $2"_zv);
+        // Получение списка рекордов с пагинацией
+        conn->prepare(tag_get_records,
+            "SELECT id, player_name AS name, score, play_time "
+            "FROM retired_players "
+            "ORDER BY score DESC, play_time ASC, player_name ASC "
+            "LIMIT $1 OFFSET $2"_zv);
 
        /* conn->prepare(tag_get_records,
             "SELECT player_name AS id, name, score, play_time_ms / 1000.0 AS playTime "
